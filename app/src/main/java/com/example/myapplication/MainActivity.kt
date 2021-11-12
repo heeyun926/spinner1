b99d5e4e7d24bb212b34e044accadd9fa30380e3
heeyun926 <fkffkfk31@gmail.com>
2021-11-12 13:09:27 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var data = resources.getStringArray(R.array.Color)
+        // val imagedata = resources.obtainTypedArray(R.array.image_diary)
+
+        var adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data)
+        with(binding) {
+            spinner.adapter = adapter
+
+            spinner.setSelection(1) //시작 위치 지
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+
+                     val selected = data.get(position)
+                     result.text = selected
+
+                    when(position){
+                        0 -> {
+
+                        }
+
+                    }
+
+                }
+
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+
+            }
+        }
+
+
+    }
+
+
+}
